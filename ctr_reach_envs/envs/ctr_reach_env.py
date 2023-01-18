e7783edd9ad13b8aeb40fc9a377f694f306811a9
Keshav Iyengar <keshav.iyengar94@gmail.com>
2023-01-18 13:54:02 +0000

    added resample joints in reset function.

--- ctr_reach_envs/envs/ctr_reach_env.py
+++ ctr_reach_envs/envs/ctr_reach_env.py
@@ -8,17 +8,19 @@ from ctr_kinematics.TwoTubeCTR import TwoTubeCTRKinematics
 
 class CtrReachEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": 4}
+
     def __init__(self, ctr_parameters, goal_parameters, reward_type, joint_representation, noise_parameters,
-                 initial_joints, max_extension_action, max_rotation_action, steps_per_episode, n_substeps, render_mode):
+                 initial_joints, resample_joints, max_extension_action, max_rotation_action, steps_per_episode,
+                 n_substeps, render_mode):
         # Tubes ordered outermost to innermost in environment but innermost to outermost in kinematics
         self.ctr_parameters = ctr_parameters
         self.num_tubes = len(self.ctr_parameters)
-        assert self.num_tubes in [2,3]
+        assert self.num_tubes in [2, 3]
         if self.num_tubes == 2:
             self.tube_length = np.array([ctr_parameters['outer']['length'], ctr_parameters['inner']['length']])
         else:
             self.tube_length = np.array([ctr_parameters['outer']['length'], ctr_parameters['middle']['length'],
-                 ctr_parameters['inner']['length']])
+                                         ctr_parameters['inner']['length']])
         self.goal_parameters = goal_parameters
         self.reward_type = reward_type
         assert joint_representation in ['proprioceptive', 'egocentric']
@@ -28,6 +30,7 @@ class CtrReachEnv(gym.Env):
             self.joints = np.zeros(self.num_tubes * 2)
         else:
             self.joints = initial_joints
+        self.resample_joints = resample_joints
         self.max_extension_action = max_extension_action
         self.max_rotation_action = max_rotation_action
         self.steps_per_episode = steps_per_episode
@@ -77,8 +80,11 @@ class CtrReachEnv(gym.Env):
     def reset(self, seed=None, options=None):
         # TimeLimit wrapper to ensure end of episode is handled correctly
         self.desired_goal = self.kinematics.forward_kinematics(flip_joints(sample_joints(self.tube_length)))
+        if self.resample_joints:
+            self.joints = sample_joints(self.tube_length)
         self.achieved_goal = self.kinematics.forward_kinematics(flip_joints(self.joints))
-        obs = get_obs(self.joints, self.joint_representation, self.desired_goal, self.achieved_goal, self.goal_tolerance,
+        obs = get_obs(self.joints, self.joint_representation, self.desired_goal, self.achieved_goal,
+                      self.goal_tolerance,
                       self.tube_length)
         return obs, {'achieved_goal': self.achieved_goal, 'desired_goal': self.desired_goal}
 
@@ -101,7 +107,7 @@ class CtrReachEnv(gym.Env):
 
     @staticmethod
     def compute_distance(achieved_goal, desired_goal):
-        #assert achieved_goal.shape == desired_goal.shape
+        # assert achieved_goal.shape == desired_goal.shape
         d = np.linalg.norm(achieved_goal - desired_goal, axis=-1)
         return d
 
@@ -120,27 +126,33 @@ class CtrReachEnv(gym.Env):
             self.visualization.close()
             self.visualization = None
 
+
 def test_subproc_vec_env(num_envs):
     from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
     from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
     from stable_baselines3.common.env_util import make_vec_env
     spec = gym.spec('CTR-Reach-v0')
     kwargs = dict()
+
     def make_env():
         env = spec.make(**kwargs)
         return env
+
     return make_vec_env(make_env, n_envs=num_envs, vec_env_cls=SubprocVecEnv)
+
 
 def regular_env():
     spec = gym.spec('CTR-Reach-v0')
     kwargs = dict()
     return spec.make(**kwargs)
 
+
 if __name__ == '__main__':
     import ctr_reach_envs
+
     num_envs = 1
     env = regular_env()
-    #env = test_subproc_vec_env(num_envs)
+    # env = test_subproc_vec_env(num_envs)
     for _ in range(10):
         env.reset()
         for _ in range(10):
