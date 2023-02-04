e2fa57377acf9dab1a7b19e469650bf53602f12f
Keshav Iyengar <keshav.iyengar94@gmail.com>
2023-02-04 14:49:11 +0000

    new hardware parameters.

--- ctr_reach_envs/__init__.py
+++ ctr_reach_envs/__init__.py
@@ -5,26 +5,30 @@ register(
     id='CTR-Reach-v0', entry_point='ctr_reach_envs.envs:CtrReachEnv',
     kwargs={
         'ctr_parameters': {
-            #'inner':
+            # 'inner':
             #    {'length': 400e-3, 'length_curved': 200e-3, 'diameter_inner': 2 * 0.35e-3,
             #     'diameter_outer': 2 * 0.55e-3, 'stiffness': 70e+9, 'torsional_stiffness': 10.0e+9, 'x_curvature': 12.0,
             #     'y_curvature': 0},
-            #'outer':
+            # 'outer':
             #    {'length': 300e-3, 'length_curved': 150e-3, 'diameter_inner': 2 * 0.7e-3,
             #     'diameter_outer': 2 * 0.9e-3, 'stiffness': 70e+9, 'torsional_stiffness': 10.0e+9, 'x_curvature': 6.0,
             #     'y_curvature': 0},
+            # New hardware parameters, check stiffness and diameters
+            # New hardware parameters, check stiffness and diameters
             'inner':
-                {'length': 210.0e-3, 'length_curved': 31e-3, 'diameter_inner': 0.4e-3, 'diameter_outer': 0.5e-3,
-                 'stiffness': 50e+10, 'torsional_stiffness': 50.0e+10 / (2 * (1 + 0.3)),
-                 'x_curvature': 28.0, 'y_curvature': 0},
+                {'length': 104.7e-3, 'length_curved': 104.7e-3, 'diameter_inner': 0.508e-3,
+                 'diameter_outer': 0.66e-3, 'stiffness': 7.5e+10, 'torsional_stiffness': 2.5e+10,
+                 'x_curvature': 1 / 40.63 * 1e3, 'y_curvature': 0},
+
             'middle':
-               {'length': 165.0e-3, 'length_curved': 100e-3, 'diameter_inner': 0.7e-3, 'diameter_outer': 0.9e-3,
-                'stiffness': 50e+10, 'torsional_stiffness': 50.0e+10 / (2 * (1 + 0.3)),
-                'x_curvature': 12.4, 'y_curvature': 0},
+                {'length': 50.0e-3, 'length_curved': 50.0e-3, 'diameter_inner': 0.7e-3,
+                 'diameter_outer': 1.0e-3, 'stiffness': 7.5e+10, 'torsional_stiffness': 2.5e+10,
+                 'x_curvature': 1 / 52.3 * 1e3, 'y_curvature': 0},
+
             'outer':
-                {'length': 110.0e-3, 'length_curved': 100e-3, 'diameter_inner': 1.2e-3, 'diameter_outer': 1.5e-3,
-                 'stiffness': 50e+10, 'torsional_stiffness': 50.0e+10 / (2 * (1 + 0.3)),
-                 'x_curvature': 4.37, 'y_curvature': 0}
+                {'length': 22.0e-3, 'length_curved': 22.0e-3, 'diameter_inner': 1.15e-3,
+                 'diameter_outer': 1.63e-3, 'stiffness': 7.5e+12, 'torsional_stiffness': 2.5e+12,
+                 'x_curvature': 1 / 71.23 * 1e3, 'y_curvature': 0}
         },
         'goal_parameters': {
             'final_tol': 0.001, 'initial_tol': 0.020, 'function_steps': 500000, 'function_type': 'constant'
@@ -48,17 +52,19 @@ register(
     kwargs={
         'ctr_parameters': {
             'inner':
-                {'length': 210.0e-3, 'length_curved': 31e-3, 'diameter_inner': 0.4e-3, 'diameter_outer': 0.5e-3,
-                 'stiffness': 50e+10, 'torsional_stiffness': 50.0e+10 / (2 * (1 + 0.3)),
-                 'x_curvature': 28.0, 'y_curvature': 0},
+                {'length': 104.7e-3, 'length_curved': 104.7e-3, 'diameter_inner': 0.508e-3,
+                 'diameter_outer': 0.66e-3, 'stiffness': 7.5e+10, 'torsional_stiffness': 2.5e+10,
+                 'x_curvature': 1 / 40.63 * 1e3, 'y_curvature': 0},
+
             'middle':
-                {'length': 165.0e-3, 'length_curved': 100e-3, 'diameter_inner': 0.7e-3, 'diameter_outer': 0.9e-3,
-                 'stiffness': 50e+10, 'torsional_stiffness': 50.0e+10 / (2 * (1 + 0.3)),
-                 'x_curvature': 12.4, 'y_curvature': 0},
+                {'length': 50.0e-3, 'length_curved': 50.0e-3, 'diameter_inner': 0.7e-3,
+                 'diameter_outer': 1.0e-3, 'stiffness': 7.5e+10, 'torsional_stiffness': 2.5e+10,
+                 'x_curvature': 1 / 52.3 * 1e3, 'y_curvature': 0},
+
             'outer':
-                {'length': 110.0e-3, 'length_curved': 100e-3, 'diameter_inner': 1.2e-3, 'diameter_outer': 1.5e-3,
-                 'stiffness': 50e+10, 'torsional_stiffness': 50.0e+10 / (2 * (1 + 0.3)),
-                 'x_curvature': 4.37, 'y_curvature': 0}
+                {'length': 22.0e-3, 'length_curved': 22.0e-3, 'diameter_inner': 1.15e-3,
+                 'diameter_outer': 1.63e-3, 'stiffness': 7.5e+12, 'torsional_stiffness': 2.5e+12,
+                 'x_curvature': 1 / 71.23 * 1e3, 'y_curvature': 0}
         },
         'goal_parameters': {
             'final_tol': 0.001, 'initial_tol': 0.020, 'function_steps': 500000, 'function_type': 'constant'
